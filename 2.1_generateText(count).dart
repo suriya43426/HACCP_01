2e160bad5d8b5d6a3f3fcb8b616b913d2172422c
suriya chayatummagoon <42575704+suriya43426@users.noreply.github.com>
2022-01-06 10:51:10 +0700

    Create 2.1_generateText(count).dart

---
+++ 2.1_generateText(count).dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  List<Widget> generateText(int count) {
+    List<Widget> textWidgets = [];
+    for (var i = 0; i < count; i++) {
+      var widget = Text('${i+1}');
+      textWidgets.add(widget);
+    }
+    return textWidgets;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("AIML/HACCP"),
+      ),
+      body: Column(
+        children: generateText(12),
+      ),
+    );
+  }
+}
