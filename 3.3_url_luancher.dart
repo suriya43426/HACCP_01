6fdfcdb4766f236e6dcba9668d081503e1ced7c7
suriya chayatummagoon <42575704+suriya43426@users.noreply.github.com>
2022-01-06 23:17:09 +0700

    Create 3.3_url_luancher.dart

---
+++ 3.3_url_luancher.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    List<Contact> contactList = [
+      Contact('Suriya1', '080-443-2496', 'assets/mitrphol.png'),
+      Contact('Yasinee', '080-442-2344', 'assets/mitrphol.png'),
+      Contact('Suriya1', '080-443-2496', 'assets/mitrphol.png'),
+      Contact('Yasinee', '080-442-2344', 'assets/mitrphol.png'),
+      Contact('Suriya1', '080-443-2496', 'assets/mitrphol.png'),
+      Contact('Yasinee', '080-442-2344', 'assets/mitrphol.png'),
+      Contact('Suriya1', '080-443-2496', 'assets/mitrphol.png'),
+      Contact('Yasinee', '080-442-2344', 'assets/mitrphol.png'),
+      Contact('Suriya1', '080-443-2496', 'assets/mitrphol.png'),
+      Contact('Yasinee', '080-442-2344', 'assets/mitrphol.png'),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("AIML/HACCP"),
+      ),
+      // Constructor Method: Builder, ListView
+      body: ListView.builder(
+        itemCount: contactList.length,
+        itemBuilder: (BuildContext context, int index) {
+
+          Contact contact = contactList[index];
+
+          return ListTile(
+            leading: Image.asset(contact.imageSource),
+            title: Text(contact.name),
+            subtitle: Text(contact.phoneNumber),
+            onTap: (){
+              // pub.dev url_launcher
+              print('${contact.phoneNumber}');
+              launch('tel: ${contact.phoneNumber}');
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class Contact{
+  String name;
+  String phoneNumber;
+  String imageSource;
+
+  // same as Constructor
+  Contact(this.name, this.phoneNumber, this.imageSource);
+}
