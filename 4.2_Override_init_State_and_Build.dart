d4e5d6ee83f8ed3e3b4c02806c34b46bd65ff7a6
suriya chayatummagoon <42575704+suriya43426@users.noreply.github.com>
2022-01-07 09:11:48 +0700

    Create 4.2_Override_init_State_and_Build.dart

---
+++ 4.2_Override_init_State_and_Build.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    // for conntect api
+    super.initState();
+    print("init state");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    print("build");
+    
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('COVID19'),
+        ),
+        body: ListView(
+          children: [
+            // ListTile Fixed
+            ListTile(
+              title: Text("ผู้ติดเชื้อสะสม"),
+              subtitle: Text("0"),
+            ),
+            ListTile(
+              title: Text("หายแล้ว"),
+              subtitle: Text("0"),
+            ),
+            ListTile(
+              title: Text("รักษาในโรงบาล"),
+              subtitle: Text("0"),
+            ),
+            ListTile(
+              title: Text("เสียชีวิต"),
+              subtitle: Text("0"),
+            ),
+          ],
+        ));
+  }
+}
